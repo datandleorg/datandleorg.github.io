3413f73609937cf8fcebb9ea5533229367c9c65a
janesshasri <saravanas.office@gmail.com>
2020-04-21 20:34:09 +0530

    first commit

---
+++ precache-manifest.495d5eee735636f80b51cb9469fdf5c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f63501f5e58c3e0612b27fd58178378",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34641478f91fd3f9bfc5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90f2c3cb3c7f87f41a35",
+    "url": "/static/js/2.5f8059be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5f8059be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34641478f91fd3f9bfc5",
+    "url": "/static/js/main.ca50b7ed.chunk.js"
+  },
+  {
+    "revision": "6fe19b4c7e2f272d2f6b",
+    "url": "/static/js/runtime-main.313704a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
